0e6c9e1db85d9d7de588d731177f2966196eaf95
Denisexper <clasic.exper2023@gmail.com>
2026-05-17 10:03:53 -0600

    feat: add new endpoints update and delete for alumnos

--- src/controlles/alumno.controller.js
+++ src/controlles/alumno.controller.js
@@ -13,8 +13,14 @@ export const getAlumnos = async (req, res) => {
 }
 
 export const createAlumno = async (req, res) => {
+
     const { name, carnet, carrera } = req.body;
-    console.log("Body recibido:", req.body);
+
+    //validar campos obligatorios
+    if(!name || !carnet || !carrera){
+        return res.status(400).json({ message: "Faltan campos obligatorios" });
+    }
+
     try {
         const newAlumno = await Alumno.create({ name, carnet, carrera });
         return res.status(201).json({
@@ -23,5 +29,69 @@ export const createAlumno = async (req, res) => {
         });
     } catch (error) {
         res.status(400).json({ message: error.message });
+    }
+}
+
+export const updateAlumno = async (req, res) => { 
+
+    const { id } = req.params;
+    const { name, carnet, carrera } = req.body;
+    
+    // verificar si el id es de mongo
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({ message: "ID no válido" });
+    }
+    
+    //buscar alumno para ver si existe
+    const alumonoExist = await Alumno.findById(id);
+    if(!alumonoExist){
+        return res.status(404).json({ message: "Alumno no existe" });
+    }
+
+    try {
+        
+        const updatedAlumno = await Alumno.findByIdAndUpdate(id, { name, carnet, carrera }, { new: true });
+
+        return res.status(200).json({
+            msj: "Alumno actualizado correctamente",
+            data: {
+                name: updatedAlumno.name,
+                carnet: updatedAlumno.carnet,
+                carrera: updatedAlumno.carrera
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteAlumno = async (req, res) => {
+    const { id } = req.params;
+
+    // verificar si el id es de mongo
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({ message: "ID no válido" });
+    }
+    
+    //buscar alumno para ver si existe
+    const alumonoExist = await Alumno.findById(id);
+    if(!alumonoExist){
+        return res.status(404).json({ message: "Alumno no existe" });
+    }
+
+    try {
+        
+        const deletedAlumno = await Alumno.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            msj: "Alumno eliminado correctamente",
+            data: {
+                name: deletedAlumno.name,
+                carnet: deletedAlumno.carnet,
+                carrera: deletedAlumno.carrera
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 }
--- src/routes/alumno.routes.js
+++ src/routes/alumno.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAlumnos, createAlumno } from "../controlles/alumno.controller.js";
+import { getAlumnos, createAlumno, updateAlumno, deleteAlumno} from "../controlles/alumno.controller.js";
 
 const router = Router();
 
 router.get("/alumnos", getAlumnos);
 router.post("/alumnos", createAlumno);
+router.put("/alumnos/:id", updateAlumno);
+router.delete("/alumnos/:id", deleteAlumno);
 
 export default router;
